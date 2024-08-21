d8893fd7330112826f68cc0a4b949c0fdfbbfb30
Power2913 <shakti29.uts@gmail.com>
2024-08-21 15:21:05 +0530

    dnf

--- resources/views/frontend/searchlivedata.blade.php
+++ resources/views/frontend/searchlivedata.blade.php
@@ -65,7 +65,7 @@
                     <div class="ms-3 text-md font-medium" style="margin: 5%">
                         Data Not Found. Try Again!
                     </div>
-                    <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-danger" aria-label="Close">
+                    <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8" data-dismiss-target="#toast-danger" aria-label="Close">
                         <span class="sr-only">
                             Close
                         </span>
